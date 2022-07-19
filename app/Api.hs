64d7f0b6f9e283f76eed1b92d451f132f2d4c2c5
tuckpuck <tucker@tuckertriggs.com>
2022-07-19 19:55:28 +0200

    add language list for user to choose from

--- app/Api.hs
+++ app/Api.hs
@@ -8,6 +8,7 @@ import Data.Aeson
 import GHC.Generics
 import Control.Lens
 import qualified Data.Text.IO as T
+import Control.Monad (forM_)
 
 data TranslateRequest = TranslateRequest {
     q :: Text,
@@ -18,9 +19,16 @@ data TranslateRequest = TranslateRequest {
 
 instance ToJSON TranslateRequest
 
+data Languages = Languages {
+    code :: Text, 
+    name :: Text
+} deriving (Show, Generic)
+
+instance FromJSON Languages
+
 data TranslateResponse = TranslateResponse {
     translatedText :: Text
-} deriving (Generic)
+} deriving (Show, Generic)
 
 instance FromJSON TranslateResponse
 
@@ -34,9 +42,19 @@ translateText sourceLang textToTranslate targetLang = do
     }))
     pure (translatedText (rsp ^. responseBody))
 
+getLanguages :: IO [Languages]
+getLanguages = do 
+    rsp <- asJSON =<< get "https://translate.argosopentech.com/languages"
+    pure (rsp ^. responseBody)
+
 
 main :: IO ()
 main = do
+    T.putStrLn "Available languages:"
+    langs <- getLanguages
+    forM_ langs $ \lang -> do
+        result <- pure $ name lang <> ": "  <> code lang
+        T.putStrLn result
     T.putStrLn "What language do you want to translate from?"
     sourceLanguage <- T.getLine 
     T.putStrLn "What language do you want to translate to?"
